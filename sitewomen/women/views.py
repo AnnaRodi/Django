ec2e173a0247fbfd1f39b571c4220dccfb083bc5
AnnaRodi <70751968+AnnaRodi@users.noreply.github.com>
2024-09-21 18:57:29 +0300

    lesson 2.4
    add template tags 'if', 'for'

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 def index(request):  # HttpRequest
     data ={ 'title': 'Главная страница',
             'menu': menu,
+            'posts': data_db,
             }
     return render(request, 'women/index.html', context = data)
 
