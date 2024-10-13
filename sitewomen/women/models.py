a6eb42750de4a314f883edd3bbeb57cb9db57954
AnnaRodi <70751968+AnnaRodi@users.noreply.github.com>
2024-10-13 09:44:14 +0300

    4.7 Связь one-to-one (один к одному)
    4.7 Связь one-to-one (один к одному)

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -19,6 +19,8 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices = Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='women')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -34,3 +36,17 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+class Husband(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
